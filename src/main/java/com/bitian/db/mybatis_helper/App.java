f23bebb4c6824c81ae468d1fb18dbaa43bfa4ec5
juoliii <juoliii@juoliii>
2017-10-13 13:52:34 +0800

    wefwef

--- src/main/java/com/bitian/db/mybatis_helper/App.java
+++ src/main/java/com/bitian/db/mybatis_helper/App.java
@@ -1,15 +1,5 @@
 package com.bitian.db.mybatis_helper;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.ibatis.builder.xml.XMLConfigBuilder;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
-
 /**
  * Hello world!
  *
